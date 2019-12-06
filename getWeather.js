2e37055b0c4abc3777c1467eccfe50dad55880e6
Beniamin Abramowicz <beniamin.abramowicz@gmail.com>
2019-12-06 16:12:57 +0100

    Add fetch GET method
    Added function to search button that gets weather based on town written
    in text input field in JSON format

--- getWeather.js
+++ getWeather.js
@@ -1,1 +1,13 @@
+//Constants
+const APIKEY = "8ac0c51e406de21860581c6481538617";
 //Event listeners
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById('getWeather').addEventListener('click', getWeather);
+});
+
+function getWeather(){
+    let town = document.getElementById('town').value;
+    fetch('https://api.openweathermap.org/data/2.5/weather?APPID=' + APIKEY + '&units=metric&q=' + town)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
