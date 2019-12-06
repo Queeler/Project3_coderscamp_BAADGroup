e14fba6ed0b425c2d734ef4abfc996a5617e7262
Beniamin Abramowicz <beniamin.abramowicz@gmail.com>
2019-12-06 19:36:48 +0100

    Add input field with suggestions
    Using fetch API to get town names from cityList.json file and put those
    names as option tags inside datalist tag creating suggestions when user
    writes town name

--- getWeather.js
+++ getWeather.js
@@ -3,6 +3,16 @@ const APIKEY = "8ac0c51e406de21860581c6481538617";
 //On page load
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById('getWeather').addEventListener('click', getWeather);
+
+    fetch('cityList.json')
+    .then((res) => res.json())
+    .then((data) => {
+        let output = '';
+        data.forEach(function(town){
+            output += `<option class="opstyle" value=\"${town.name}\">`;
+        });
+        document.getElementById('towns').innerHTML = output;
+    });
 });
 
 function getWeather(){
