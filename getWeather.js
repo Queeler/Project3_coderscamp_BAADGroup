b3e4bbdc2669698e2ca9be49c89baa837a91b672
Beniamin Abramowicz <beniamin.abramowicz@gmail.com>
2019-12-07 18:32:40 +0100

    Add suggestions when filling town input field
    Modified get weather form - switched select and data list for input text
    with suggestions list made of div elements. Suggestions appear depending
    on written letters (first 15 found)

--- getWeather.js
+++ getWeather.js
@@ -1,23 +1,50 @@
 //Constants
 const APIKEY = "8ac0c51e406de21860581c6481538617";
 //On page load
+let citiesList;
+
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById('getWeather').addEventListener('click', getWeather);
-
-    fetch('cityList.json')
-    .then((res) => res.json())
-    .then((data) => {
-        let output = '';
-        data.forEach(function(town){
-            output += `<option class="opstyle" value=\"${town.name}\">`;
-        });
-        document.getElementById('towns').innerHTML = output;
-    });
+    document.getElementById('town').addEventListener('keyup', populateSuggestions);
+    getCitiesList();   
 });
 
 function getWeather(){
-    let town = document.getElementById('town').value;
-    fetch('https://api.openweathermap.org/data/2.5/weather?APPID=' + APIKEY + '&units=metric&q=' + town)
+    let townField = document.getElementById('town').value;
+    fetch('https://api.openweathermap.org/data/2.5/weather?APPID=' + APIKEY + '&units=metric&q=' + townField)
     .then((res) => res.json())
     .then((data) => console.log(data));
+}
+
+function populateSuggestions(){
+    document.getElementById('towns').innerHTML = '';
+    let townField = document.getElementById('town').value;
+    let counter = 0;
+    let output = '';
+        for(var i = 0; i < citiesList.length; i++){
+            if(citiesList[i].name.substr(0, townField.length).toUpperCase() == townField.toUpperCase()){
+                output += `<div onclick=\"fillTextbox(this)\"><strong>${citiesList[i].name}</strong><input type=\"hidden\" value=\"${citiesList[i].name}\"></div>`;
+                counter++;
+            }
+            if(counter === 14){
+                break;
+            }
+        }
+    document.getElementById('towns').innerHTML = output;
+}
+
+function fillTextbox(el){
+    document.getElementById('town').value = el.getElementsByTagName('input')[0].value;
+    document.getElementById('towns').innerHTML = '';
+}
+
+async function fetchCities(){
+    let response = await fetch('cityList.json');
+    let data = response.json();
+    return data;
+}
+
+async function getCitiesList(){
+    let dataset = await fetchCities();
+    citiesList = dataset;
 }
