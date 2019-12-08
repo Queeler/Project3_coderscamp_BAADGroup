78cf177579ec0a22c9882393f7e5059acba5dcc0
Beniamin Abramowicz <beniamin.abramowicz@gmail.com>
2019-12-08 21:51:49 +0100

    Last changes

--- speechsynthesis.js
+++ speechsynthesis.js
@@ -2,7 +2,8 @@
 let Data;
 const speakButton = document.querySelector('#readWeather');
 function toggle (){
-  Data = getWeather();
+  Data = testData;
+  console.log(Data);
   const msg = new SpeechSynthesisUtterance();
   msg.lang = 'en-US'
   msg.text = `Weather for today for the city of ${Data[0]}. The temperature is ${Data[1]} °C and the pressure is ${Data[2]} hektopascals. The air humidity is ${Data[3]} percent.`;
